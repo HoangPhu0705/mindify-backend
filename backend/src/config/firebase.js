8760c735a79aef52706e72a0315eced740012ccf
HoangPhu0705 <phuhoang07051003@gmail.com>
2024-07-18 21:49:04 +0700

    again

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const admin = require('firebase-admin');
 const {getFirestore} = require('firebase-admin/firestore')
-const serviceAccount = require("../../etc/secrets/mindify-d1d55-firebase-adminsdk-fudzk-1464986a0d.json"); //service account key JSON file
+const serviceAccount = require("/etc/secrets/mindify-d1d55-firebase-adminsdk-fudzk-1464986a0d.json"); //service account key JSON file
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: 'mindify-d1d55.firebaseapp.com' // database URL
